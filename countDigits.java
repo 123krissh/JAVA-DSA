394f400efec08eb231b866c24a8ca5b8a6965972
123krissh <krisshsuthar348@gmail.com>
2025-01-03 21:12:42 +0530

    Questions

--- countDigits.java
+++ countDigits.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 public class countDigits {
     public static void main(String[] args) {
-        // Scanner sc = new Scanner(System .in);
-        // int n = sc.nextInt();
-        // int count = 0;
-        // if(n==0) count++;
-        // while(n!=0){
-        //     n/=10;
-        //     count++;
-        // }
-        // System.out.println(count);
+        Scanner sc = new Scanner(System .in);
+        int n = sc.nextInt();
+        int count = 0;
+        if(n==0) count++;
+        while(n!=0){
+            n/=10;
+            count++;
+        }
+        System.out.println(count);
 
 
        // sum of digits
-    //    Scanner sc = new Scanner(System .in);
-    //    int n = sc.nextInt();
-    //    int sum = 0;
-    //    while (n!=0) {
-    //     int lastDigit = n%10;
-    //     sum += lastDigit;
-    //     n/=10;
-    //    }
-    //    System.out.println(sum);
+       Scanner sc = new Scanner(System .in);
+       int n = sc.nextInt();
+       int sum = 0;
+       while (n!=0) {
+           int lastDigit = n%10;
+           sum += lastDigit;
+           n/=10;
+       }
+       System.out.println(sum);
 
 
     // reverse of a digit
@@ -35,7 +35,6 @@ public class countDigits {
         n/=10;
     }
     System.out.println(rev);
-
 
     }
 }
