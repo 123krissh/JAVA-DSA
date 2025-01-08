4811d729b4292149322721abfecc9f3878c3edfb
Krishna Suthar <krisshsuthar348@gmail.com>
2025-01-09 01:17:01 +0530

    patterns.java

--- Patterns/patterns.java
+++ Patterns/patterns.java
@@ -1,3 +1,5 @@
+package Patterns;
+
 import java.util.Scanner;
 
 public class patterns {
@@ -7,8 +9,15 @@ public class patterns {
         int n = sc.nextInt();
         for(int i = 1;i<=n;i++){ 
             for(int j=1;j<=n;j++){
-                System.out.print(i+" ");
-                // System.out.print(j+" ");
+                System.out.print(j+" ");
+
+                // same no. print in same rwo
+                // System.out.print(i+" ");
+
+                // print pattern of alphabet
+                // System.out.print((char)(j+64)+" ");
+
+                // System.out.print((char)(i+64)+" ");
             }
             System.out.println();
         }
