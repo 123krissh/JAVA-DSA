6167a9d565816b083714242abf85c813cdbd3b7a
123krissh <krisshsuthar348@gmail.com>
2025-07-25 19:09:26 +0530

    sudoku

--- Backtracking/sudokuSolver.java
+++ Backtracking/sudokuSolver.java
@@ -31,7 +31,7 @@ public class sudokuSolver {
 
     public static boolean sudokuSolver(int sudoku[][], int row, int col) {
         // base case
-        if(row == 9 && col == 0) {
+        if(row == 9) {
             return true;
         }
         // recursion
@@ -68,15 +68,16 @@ public class sudokuSolver {
     }
     
     public static void main(String[] args) {
-        int sudoku[][] = { {0, 0, 8, 0, 0, 0, 0, 0, 0},
-        {4, 9, 0, 1, 5, 7, 0, 0, 2},
-        {0, 0, 3, 0, 0, 4, 1, 9, 0},
-        {1, 8, 5, 0, 6, 0, 0, 2, 0},
-        {0, 0, 0, 0, 2, 0, 6, 0, 0},
-        {9, 6, 0, 4, 0, 5, 3, 0, 0},
-        {0, 3, 0, 0, 7, 2, 0, 0, 4},
-        {0, 4, 9, 0, 3, 0, 0, 5, 7},
-        {8, 2, 7, 0, 0, 9, 0, 1, 3} };
+        int sudoku[][] = {
+    {5, 3, 0, 0, 7, 0, 0, 0, 0},
+    {6, 0, 0, 1, 9, 5, 0, 0, 0},
+    {0, 9, 8, 0, 0, 0, 0, 6, 0},
+    {8, 0, 0, 0, 6, 0, 0, 0, 3},
+    {4, 0, 0, 8, 0, 3, 0, 0, 1},
+    {7, 0, 0, 0, 2, 0, 0, 0, 6},
+    {0, 6, 0, 0, 0, 0, 2, 8, 0},
+    {0, 0, 0, 4, 1, 9, 0, 0, 5},
+    {0, 0, 0, 0, 8, 0, 0, 7, 9} };
 
         if(sudokuSolver(sudoku, 0, 0)) {
             System.out.println("Solution Exists");
