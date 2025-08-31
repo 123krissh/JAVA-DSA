7111c4ad935780b8363957f87b17526b040c24fc
123krissh <krisshsuthar348@gmail.com>
2025-09-01 00:05:44 +0530

    sudoku

--- Backtracking/sudokuSolver.java
+++ Backtracking/sudokuSolver.java
@@ -86,4 +86,47 @@ public class sudokuSolver {
             System.out.println("Solution dose not exist");
         }
     }
+
+    // leetcode ----->
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] rows = new boolean[9][9];  
+        boolean[][] cols = new boolean[9][9];   
+        boolean[][] boxes = new boolean[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    int num = c - '1'; 
+                    int boxIndex = (i / 3) * 3 + (j / 3); 
+
+                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
+                        return false;
+                    }
+                    rows[i][num] = true;
+                    cols[j][num] = true;
+                    boxes[boxIndex][num] = true;
+                }
+            }
+        }
+
+    return true;
+
+    // HashSet<String> seen = new HashSet<>();
+    // for (int i = 0; i < 9; i++) {
+    //     for (int j = 0; j < 9; j++) {
+    //         char num = board[i][j];
+    //         if (num != '.') {
+    //             String rowKey = num + " in row " + i;
+    //             String colKey = num + " in col " + j;
+    //             String boxKey = num + " in box " + (i / 3) + "-" + (j / 3);
+
+    //             if (!seen.add(rowKey) || !seen.add(colKey) || !seen.add(boxKey)) {
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    // }
+    // return true;
+    }
 }
