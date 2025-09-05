75d200e17a552bd061c2fab59784d803cf9288db
123krissh <krisshsuthar348@gmail.com>
2025-09-06 01:22:25 +0530

    make integer zero using bitCount

--- BitManipulation/countSetBit.java
+++ BitManipulation/countSetBit.java
@@ -1,5 +1,5 @@
 public class countSetBit {
-    public static int countSetBits(int n){
+    public static int countSetBits(long n){
         int count = 0;
         while (n > 0) {
             if ((n & 1) == 1) {
@@ -10,7 +10,21 @@ public class countSetBit {
         return count;
     }
 
+    // java built in method fot count set bits : datatype.bitCount(n) -> long.bitCount(n)
+
+    public static int makeTheIntegerZero(int num1, int num2) {
+        for(int k=0; k<=60; k++){
+            long x = (long) num1 - (long) k * num2;
+            
+            if(x<k) continue;
+            int bits = countSetBits(x);
+            if(bits<=k && x>=k) return k;
+        }
+        return -1;
+    } 
+
     public static void main (String[] args) {
         System.out.println(countSetBits(10));
+        System.out.println(makeTheIntegerZero(10, 2));
     }
 }
