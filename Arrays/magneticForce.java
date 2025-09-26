3adbc2b1948ac2c754fe90429847074f3748223f
Krishna Suthar <krisshsuthar348@gmail.com>
2025-09-26 20:12:26 +0530

    minimized maximum problems

--- Arrays/magneticForce.java
+++ Arrays/magneticForce.java
@@ -39,9 +39,41 @@ public class magneticForce {
         return false;
     }
 
+    // Minimized Maximum of Products distribute to any store ---->
+    public static int minimizedMaximum(int n, int[] quantities) {
+        // Arrays.sort(quantities);
+        int L = 1;
+        int H = 0;
+        
+        for (int q : quantities) {
+            H = Math.max(H, q);
+        }
+      
+        while(L <= H){
+            int mid = (L+H)/2;
+            if(canDistribute(n, quantities, mid)){
+                H = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+        return L;
+    }
+
+    public static boolean canDistribute(int n, int[] quantities, int x) {
+        int store = 0;
+        for(int q: quantities){
+            store += (q + x - 1) / x; 
+        }
+        return store <= n;
+    }
+
     public static void main(String[] args) {
         int[] position = {1,2,3,4,7};
         int m = 3;
         System.out.println(maxDistance(position, m));
+        int[] quantities = {1,2,3,4,7};
+        int n = 3;
+        System.out.println(minimizedMaximum(n, quantities));
     }
 }
