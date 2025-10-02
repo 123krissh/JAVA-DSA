48dfe69ec8eac55cc0857c957c6467570372ca97
Krishna Suthar <krisshsuthar348@gmail.com>
2025-10-02 17:01:23 +0530

    waterBottle2

--- waterBottle.java
+++ waterBottle.java
@@ -16,9 +16,29 @@ public class waterBottle {
         return totalDrink; 
     }
 
+    public static int maxBottlesDrunk(int numBottles, int numExchange) {
+        int totalDrunk = 0;
+        int currEmptyBottle = 0;
+        int currFullBottle = numBottles;
+
+        while(currFullBottle > 0){
+            totalDrunk += currFullBottle;
+            currEmptyBottle += currFullBottle;
+            currFullBottle = 0;
+
+            if(currEmptyBottle >= numExchange){
+                currEmptyBottle -= numExchange;
+                numExchange++;
+                currFullBottle++;
+            }
+        }
+        return totalDrunk;
+    }
+
     public static void main(String[] args) {
         int numBottles = 15;
         int numExchange = 4;
         System.out.println("Total drunk water bottles: " + numWaterBottles(numBottles, numExchange));
+        System.out.println("Max Bottles Drunk: " + maxBottlesDrunk(numBottles, numExchange));
     }
 }
