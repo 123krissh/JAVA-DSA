3bbb4a0c8e66f3b35c04679768e266d01c99998a
Krishna Suthar <krisshsuthar348@gmail.com>
2025-10-14 02:12:42 +0530

    Anagram

--- Questions/anagram.java
+++ Questions/anagram.java
@@ -27,7 +27,6 @@ public class anagram {
             return false;
         }
     }
-
     // OR
     public static String sortString(String s){
         char[] strArr = s.toCharArray();
@@ -45,11 +44,56 @@ public class anagram {
             }
         }
         return list;
-    }   
+    }  
+    
+    // Group Anagrams ---->
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    // public static List<List<String>> groupAnagrams(String[] strs) {
+    //     Map<Integer, List<String>> map = new HashMap<>();
+    //     boolean[] visited = new boolean[strs.length];
+    //     List<List<String>> ans = new ArrayList<>();
+        
+    //     for (int i = 0; i < strs.length; i++) {
+    //         if (visited[i]) continue; 
+
+    //         List<String> group = new ArrayList<>();
+    //         group.add(strs[i]);
+    //         visited[i] = true;
+
+    //         for (int j = i + 1; j < strs.length; j++) {
+    //             if (!visited[j] && isAnagram(strs[i], strs[j])) {
+    //                 group.add(strs[j]);
+    //                 visited[j] = true;
+    //             }
+    //         }
+    //         map.put(i, group);
+    //     }
+    //     for (List<String> list : map.values()) {
+    //         ans.add(new ArrayList<>(list));
+    //     }
+    //     return ans;
+    // }
     
     public static void main(String[] args) {
         String[] words = {"abba","baba","bbaa","cd","cd"};
         List<String> result = removeAnagrams2(words);
         System.out.println(result);
+
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        List<List<String>> grpAnagrams = groupAnagrams(strs);
+        System.out.println(grpAnagrams);
     }
 }
