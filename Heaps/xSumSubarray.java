18fb7bc2a3f7584e5d9ad3bce7416f0b9fa660e8
Krishna Suthar <krisshsuthar348@gmail.com>
2025-11-05 00:20:33 +0530

    XSUM with priorityQueue

--- Heaps/xSumSubarray.java
+++ Heaps/xSumSubarray.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class xSumSubarray {
 
@@ -16,7 +17,7 @@ public class xSumSubarray {
             freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
         }
 
-        res[0] = xSum(freq, x);
+        res[0] = getXSum(freq, x);
 
         for(int i=k; i<n; i++) {
             int newEle = nums[i];
@@ -27,7 +28,7 @@ public class xSumSubarray {
             if(freq.getOrDefault(oldEle, 0) == 0) {
                 freq.remove(oldEle);
             }
-            res[i - k + 1] = xSum(freq, x);
+            res[i - k + 1] = getXSum(freq, x);
         }
         return res;
     }
@@ -54,8 +55,28 @@ public class xSumSubarray {
         return sum;
     }
 
+    // with priority Queue 
+    private static int getXSum(Map<Integer, Integer> freq, int x) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> { // minHeap
+            if (a[1] != b[1]) return a[1] - b[1]; // smaller freq → weaker
+            return a[0] - b[0]; // smaller num → weaker
+        });
+
+        for (var entry : freq.entrySet()) {
+            pq.offer(new int[]{entry.getKey(), entry.getValue()});
+            if (pq.size() > x) pq.poll(); // remove weakest
+        }
+
+        int sum = 0;
+        while (!pq.isEmpty()) {
+            int[] top = pq.poll();
+            sum += top[0] * top[1];
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
-        int[] nums = {1,2,2,3,1,4,2};
+        int[] nums = {1,1,2,2,3,4,2,3};
         int k = 6;
         int x = 2;
 
