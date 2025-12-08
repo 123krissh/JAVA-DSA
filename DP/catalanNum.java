d160f93a26ad40e207ea6868e4c09e40491f6b76
Krishna Suthar <krisshsuthar348@gmail.com>
2025-12-09 00:45:38 +0530

    DP(catalan, mountain, countBST)

--- DP/catalanNum.java
+++ DP/catalanNum.java
@@ -37,6 +37,44 @@ public class catalanNum {
         return dp[n];
     }
 
+    // catalan variations: 
+    // count BST with n nodes
+    // count valid parentheses with n pairs
+    // count mountain ranges with n upstrokes
+
+    // countBST
+    public static int countBST(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n+1; i++) {
+            // C1 -> BST (i node) -> dp[i]
+            for(int j=0; j<i; j++) {
+                int left = dp[j];
+                int right = dp[i-j-1];
+                dp[i] += left * right;
+            }
+        }
+        return dp[n];
+    }
+    // mountain Ranges
+    public static int mountainRanges(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<n+1; i++) {
+            // i pairs -> mountain ranges -> Ci
+            for(int j=0; j<i; j++) {
+                int inside = dp[j];
+                int outside = dp[i-j-1];
+                dp[i] += inside * outside; // ci = cj*ci-j-1
+            }
+        } 
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 5;
         System.out.println(CatalanRec(n));
@@ -45,5 +83,8 @@ public class catalanNum {
         Arrays.fill(dp, -1);
         System.out.println(CatalanMemo(n, dp));
         System.out.println(CatalanTab(n));
+
+        System.out.println(countBST(n));
+        System.out.println(mountainRanges(n));
     }
 }
